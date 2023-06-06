3c9f27284911f97dc38f5036d4fa354cd2f1c6ed
CMLTTN <cemaldim@gmail.com>
2023-06-06 22:20:41 +0300

    index +cards

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -4,18 +4,17 @@ import PlayerCard from "./PlayerCard";
 
 const CardContainer = () => {
   return (
-    <Container>
-      <Row>
-         {data.map((player, i) => {
-          return(
-               <Col>
-
-               <PlayerCard {...player}/>
-               </Col>
-          )
-         })}
+    <Container className="card-container rounded-4 my-4 p-3">
+      <Row className="g-3 justify-content-center">
+        {data.map((player, i) => {
+          return (
+            <Col md={6} lg={6} xl={3} key={i}>
+              <PlayerCard {...player} />
+            </Col>
+          );
+        })}
       </Row>
-      <PlayerCard />
+      
     </Container>
   );
 };
--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,13 +1,12 @@
  import Card from "react-bootstrap/Card"
  
- const PlayerCard = () => {
+ const PlayerCard = ({name, img, statistics}) => {
    return (
-     <Card>
-       <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+     <Card className="rounded-2 m-auto player-card" role="button" >
+       <Card.Img variant="top" src={img} className="player-logo" />
        <Card.Footer>
-         <Card.Title>Card Link</Card.Title>
+         <Card.Title>{name}</Card.Title>
        </Card.Footer>
      </Card>
-   );
- };
- export default PlayerCard;
+   )}
+export default PlayerCard;
